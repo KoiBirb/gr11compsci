f94276df629e6a5b6d73f1b92296ec0453eb1be5
KoiBirb <centralleo474@gmail.com>
2024-02-15 08:29:15 -0500

    Lesson 1!

---
+++ U1/Lesson 1-Intro/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        String happy = "Happy birthday to you! ";
+        String name = "Cedric! ";
+        System.out.println(happy + happy + "Happy birthday dear, " + name + happy);
+    }
+}
