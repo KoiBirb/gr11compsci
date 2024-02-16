b7b5f4dbf805b20f692f4a4c73c73c1aada1de9e
KoiBirb <centralleo474@gmail.com>
2024-02-16 08:23:27 -0500

    lesson 2

--- U1/Lesson2/src/Main.java
+++ U1/Lesson2/src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello forntite!");
+
+        String name = "Leo";
+        int numSiblings = 0;
+        String hobby = "video games";
+        char symbol = '!';
+        double number = 3.14;
+
+
+        System.out.println("Hello!");
+        System.out.println("My name is " + name);
+        System.out.println("I have " + numSiblings + " siblings");
+        System.out.println("I like to play " + hobby);
+        System.out.println("My favourite symbol is " + symbol);
+        System.out.println("My favourite number is " + number);
+
     }
 }
