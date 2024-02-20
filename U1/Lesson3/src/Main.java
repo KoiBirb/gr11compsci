a7294af084aa50faa00ea2f3a0078ca9442db59e
KoiBirb <centralleo474@gmail.com>
2024-02-20 08:42:15 -0500

    lesson 3

---
+++ U1/Lesson3/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("""
+                Hello!
+                My name is Henry Martell
+                I have 3 sisters and 4 brothers
+                I play baseball and hockey
+                My favourite band is Metallica       
+                """);
+
+        System.out.println("CLASS SCHEDULE\nP1\tComputer Science\nP2\tEnglish\nP3\tMusic\nP4\tBiology");
+
+        System.out.println("\\(\")/");
+        System.out.println("  ==");
+        System.out.println("  ||");
+        System.out.println("  /\\");
+        System.out.println("_/  \\_");
+    }
+}
