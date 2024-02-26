94179e335350c29a1a467d92e16bf44823ad611b
KoiBirb <centralleo474@gmail.com>
2024-02-26 08:25:42 -0500

    Lesson 6!

---
+++ U1/Lesson6/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Distance traveled (in km): ");
+        double distance = scanner.nextDouble();
+        System.out.print("Time taken (in hours): ");
+        double time = scanner.nextDouble();
+        System.out.println("Average speed: " + distance / time + " km/h");
+    }
+}
